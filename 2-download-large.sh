a1080bff69d4d96a693fdea3896533f714721507
Ali Zaidi <alikaz.zaidi@gmail.com>
2017-03-02 15:30:07 +0000

    fix extract gz

--- 2-download-large.sh
+++ 2-download-large.sh
@@ -1,9 +1,11 @@
-wget 'https://marinchshare1.blob.core.windows.net/weather1/WeatherRaw.tgz?sv=2014-02-14&sr=c&sig=Vhfq%2BSWZKHuzVzHATd4ij0Cp8nVF%2B%2FkBANm2JyrC1ho%3D&st=2016-04-06T07%3A00%3A00Z&se=2017-04-07T07%3A00%3A00Z&sp=r' -o WeatherRaw.tgz
+wget 'https://marinchshare1.blob.core.windows.net/weather1/WeatherRaw.tgz?sv=2014-02-14&sr=c&sig=Vhfq%2BSWZKHuzVzHATd4ij0Cp8nVF%2B%2FkBANm2JyrC1ho%3D&st=2016-04-06T07%3A00%3A00Z&se=2017-04-07T07%3A00%3A00Z&sp=r'
 
 wget http://packages.revolutionanalytics.com/datasets/AirOnTime87to12/AirOnTimeCSV.zip
 
-unzip AirOnTimeCSV.zip
-tar -xvfz WeatherRaw.tgz
+sudo apt-get install p7zip-full
+
+7za x AirOnTimeCSV.zip
+tar -xvzf 'WeatherRaw.tgz?sv=2014-02-14&sr=c&sig=Vhfq%2BSWZKHuzVzHATd4ij0Cp8nVF%2B%2FkBANm2JyrC1ho%3D&st=2016-04-06T07%3A00%3A00Z&se=2017-04-07T07%3A00%3A00Z&sp=r'
 
 hdfs dfs -mkdir /FlightsLarge/
 hdfs dfs -copyFromLocal WeatherRaw/ /FlightsLarge/
