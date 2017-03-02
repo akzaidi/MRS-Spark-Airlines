7264a05491b90c707b63963a157861eb9681bb0c
Ali Zaidi <alikaz.zaidi@gmail.com>
2017-03-02 05:31:17 +0000

    update rstudio-server

--- 1-download-air.sh
+++ 1-download-air.sh
@@ -13,3 +13,7 @@ hdfs dfs -copyFromLocal AirlineSubsetCsv/ /FlightData/
 hdfs dfs -copyFromLocal WeatherSubsetCsv /FlightData/
 
 Rscript ./install-pkgs.R
+
+wget https://raw.githubusercontent.com/akzaidi/etc/master/inst/install-rstudio-ubuntu.sh
+chmod +x ./install-rstudio-ubuntu.sh
+./install-rstudio-ubuntu.sh
